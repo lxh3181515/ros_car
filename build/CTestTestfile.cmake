8d13625b025a0b4104d73c30b423a1e2272b88ee
lxh3181515 <1622530287@qq.com>
2022-05-01 14:27:15 +0800

    serial, tf

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lxhong/my_workspace/ros_car/src
-# Build directory: /home/lxhong/my_workspace/ros_car/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("base_control")
--- build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/lxhong/my_workspace/ros_car/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
